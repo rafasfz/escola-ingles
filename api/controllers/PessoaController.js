efe0572c5bd99b41c60d71090457c8dc195aa957
Rafael Freire <rafaelsilvafreire2@gmail.com>
2020-08-25 00:01:25 -0300

    created method and routes for remove in the table Pessoas

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -33,6 +33,29 @@ class PessoaController {
             return res.status(500).json(err.message)
         }
     }
+
+    static async atualizaPessoa(req, res) {
+        const { id } = req.params
+        const novasInfos = req.body
+        try {
+            await database.Pessoas.update(novasInfos, { where: { id: Number(id) } })
+            const pessoaAtualizada = await database.Pessoas.findOne( {where: { id: Number(id) }} )
+            return res.status(200).json(pessoaAtualizada)
+        } catch (err) {
+            return res.status(500).json(err.message)
+        }
+    }
+
+    static async apagaPessoa(req, res) {
+        const { id } = req.params
+        try {
+            const pessoaRemovida = await database.Pessoas.findOne( {where: { id: Number(id) }} )
+            await database.Pessoas.destroy({ where: { id: Number(id) } })
+            return res.status(200).json(pessoaRemovida)
+        } catch (err) {
+            return res.status(500).json(err.message)
+        }
+    }
 }
 
 module.exports = PessoaController
