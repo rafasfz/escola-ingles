b08c853af076c07a3549b3f801deecba5de5d2a8
Rafael Freire <rafaelsilvafreire2@gmail.com>
2020-08-24 23:48:05 -0300

    created methods and routes for create and read in the Pessoas table

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -9,6 +9,30 @@ class PessoaController {
             return res.status(500).json(err.message)
         }
     }
+
+    static async pegaUmaPessoa(req, res) {
+        const { id } = req.params
+        try {
+            const umaPessoa = await database.Pessoas.findOne({
+                 where: {
+                      id: (Number(id)) 
+                    } 
+                })
+            return res.status(200).json(umaPessoa)
+        } catch (err) {
+            return res.status(500).json(err.message)
+        }
+    }
+
+    static async criaPessoa(req, res) {
+        const novaPessoa = req.body
+        try {
+            const novaPessoaCriada = await database.Pessoas.create(novaPessoa)
+            return res.status(200).json(novaPessoaCriada)
+        } catch (err) {
+            return res.status(500).json(err.message)
+        }
+    }
 }
 
 module.exports = PessoaController
